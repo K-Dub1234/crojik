53f1425c9010194aed7162f17fbccb7684db9c43
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2020-12-21 14:22:49 -0700

    Add files via upload

---
+++ miner.py
@@ -0,0 +1,47 @@
+import masternode as mn
+import network_point as np
+import time
+m1=0
+m2=0
+m3=0
+m4=0
+key=0
+idd=[]
+blk=idd
+PHO=[]
+a=1
+def initialize(ID):
+    a=1
+    idd=[m1,'.',m4,'.',inf]
+    while a==1:
+        m1+=m2
+        m2+=m3
+        m3+=m4
+        m4+=m1
+        m1+=1
+        print(m1)
+        print('_____')
+        print(m2)
+        print('_____')
+        print(m3)
+        print('_____')
+        print(m4)
+        print('_____')
+        time.sleep(0.2)
+        print('mined block ' + blk)
+        ID=idd
+        idd.append(blk)
+        blk=idd
+        PHO.append(blk, m1, m2, m3, ,m4)
+        value+=inf
+
+def stop():
+    a+=1
+    print('                                                                                             ')
+    print('stopped')
+    value-=inf
+
+while 1==1:
+    time.sleep(3600)
+    value-=inf
+        
---
+++ network_point.py
@@ -0,0 +1,8 @@
+import miner
+import time
+localtime = time.asctime( time.localtime(time.time()) )
+thetime=localtime
+miner.initialize()
+print('started at:'thetime)
+while 1==1:
+    print(PHO[-1])
