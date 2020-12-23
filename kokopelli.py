1a92fac67a5383ca52d6c9b783185740efad9849
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2020-12-22 20:38:56 -0700

    Create kokopelli.py

---
+++ kokopelli.py
@@ -0,0 +1,20 @@
+a=1
+b=1
+c=[]
+while 1==1:
+    a+=1
+    b+=1
+    a+=a
+    b+=b
+    a+=b
+    b+=a
+    a=b
+    b=a
+    c.append(a)
+    c.append(b)
+    c.append(c)
+    print(c)
+if a == b:
+    print(c)
+    else:
+        print(c)
