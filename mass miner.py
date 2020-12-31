6db10df518890e481cb9cb6e780bfd597f13410a
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2020-12-30 18:35:32 -0700

    Add files via upload

---
+++ mass miner.py
@@ -0,0 +1,104 @@
+import masternode as mn
+import network_point as np
+import time
+m1=0
+m2=0
+m3=0
+m4=0
+key=0
+idd=[]
+blk=idd
+PHO=[]
+b=1
+def initialize(ID):
+    a=1
+    idd=[m1,'.',m4,'.',inf]
+    while a==1:
+        m1+=m2
+        m2+=m3
+        m3+=m4
+        m4+=m1
+        m1+=1
+        print(m1)
+        print('_____')
+        print(m2)
+        print('_____')
+        print(m3)
+        print('_____')
+        print(m4)
+        print('_____')
+        time.sleep(0.2)
+        print('mined block ' + blk)
+        ID=idd
+        idd.append(blk)
+        blk=idd
+        PHO.append(blk, m1, m2, m3, ,m4)
+        value+=inf
+
+def stop():
+    b+=1
+    print('                                                                                             ')
+    print('stopped')
+    value-=inf
+
+while 1==1:
+    time.sleep(3600)
+    value-=inf
+
+
+DTAV = 0
+a = 1
+x = []
+def update(ip):
+	x.append(ip)
+	if IP != ip:
+		ip = IP
+		x.append(ip)
+		while True:
+			x.append(x)
+			x.append(a)
+			a += a
+			a += 1
+			a *= a
+			a *= 2
+			print(x)
+		else:
+			x.append(x)
+			x.append(a)
+			a += a
+			a += 1
+			a *= a
+			a *= 2
+			print(x)
+			print('__________________________________from override__________________________________')
+
+def dtav():
+	in1 = 0
+	in2 = 0
+	in1inf = 0
+	in2inf = 0
+	z = 1
+	beg = 0
+	end = 0
+	import random
+	while z = 1:
+		in1 = random.randint(1, 100)
+		in2 = random.randint(1, 100)
+		# beg
+                in1inf = 1
+                in1 *= in1inf
+		beg -= in1
+		in2inf = 2
+		in2 *= in2inf
+                beg += in2
+                # end
+                in1inf = 2
+                in1 *= in1inf
+		end += in2
+		in2inf = 1
+		in2 *= in2inf
+                end -= in1
+                x.append(DTAV)
+                print(DTAV)
+                DTAV = random.randint(beg, end)
+
