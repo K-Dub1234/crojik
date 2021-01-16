b98871e22e0b0b14bb9eb62a9a4436ec67c2369c
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-16 11:05:03 -0700

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,7 @@
+toIP = input("Paste or type datacenter IP adress here:")
+import __init__ as i
+i.setupPHO(frIP)
+frIP = ip
+print('••••   loading    ••••')
+ip = toIP
+print('•°•°•°•°•°•°•  ∆^∆ finished with ~cute jack-o-lantern~ process. COMPLETED.  •°•°•°•°•°•°•')
